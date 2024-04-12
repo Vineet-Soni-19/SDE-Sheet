e68afc32162c00ffcd1df5be76b54ec50ae38efa
Vineet-Soni-19 <vineet19012003@gmail.com>
2024-04-13 01:05:45 +0530

    Day 10

--- 30.DeleteGivenNode.cpp
+++ 30.DeleteGivenNode.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode *temp=node;
-        while(temp->next!=NULL){
-            int data=temp->next->val;
-            temp->val=data;
-            if(temp->next->next==NULL){
-                temp->next=NULL;
-                break;
-            }
-            temp=temp->next;
-        }
+        ListNode *temp=node->next;
+        node->val=temp->val;
+        node->next=temp->next;
+        delete(temp);
     }
 };
