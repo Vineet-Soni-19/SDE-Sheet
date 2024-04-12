610383e1763959177babe1cf023a1b65895dabf4
Vineet-Soni-19 <vineet19012003@gmail.com>
2024-04-13 00:52:25 +0530

    Day 10

--- 29.AddTwoNumbers.cpp
+++ 29.AddTwoNumbers.cpp
@@ -1,49 +1,30 @@
 class Solution {
-    ListNode* insertAtHead(ListNode* head, int data){
-        ListNode* newnode=new ListNode(data,head);
-        return newnode;
-    }
-    ListNode* reverseList(ListNode* head){
-        ListNode* pnode=NULL, *cnode=head, *nnode=NULL;
-        while(cnode!=NULL){
-            nnode=cnode->next;
-            cnode->next=pnode;
-            pnode=cnode;
-            cnode=nnode;
-        }
-        return pnode;
-    }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *first=l1, *second=l2;
         int data=0, carry=0;
-        ListNode *head=NULL;
-        while(first!=NULL && second!=NULL){
-            data=first->val+second->val+carry;
+        ListNode *head=new ListNode();
+        ListNode *curr=head;
+        while(first!=NULL || second!=NULL){
+            data=carry;
+            if(first){
+                data+=first->val;
+                first=first->next;
+            }
+            if(second){
+                data+=second->val;
+                second=second->next;
+            }
             carry=data/10;
             data=data%10;
-            head=insertAtHead(head,data);
-            first=first->next;
-            second=second->next;
-        }
-        while(first!=NULL){
-            data=first->val+carry;
-            carry=data/10;
-            data=data%10;
-            head=insertAtHead(head,data);
-            first=first->next;
-        }
-        while(second!=NULL){
-            data=second->val+carry;
-            carry=data/10;
-            data=data%10;
-            head=insertAtHead(head,data);
-            second=second->next;
+            ListNode *newnode=new ListNode(data);
+            curr->next=newnode;
+            curr=curr->next;
         }
         if(carry>0){
-            head=insertAtHead(head,carry);
+            ListNode *newnode=new ListNode(carry);
+            curr->next=newnode;
         }
-        head=reverseList(head);
-        return head;
+        return head->next;
     }
 };
